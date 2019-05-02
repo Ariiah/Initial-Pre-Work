8a20a35fb09cffee8844b7222f22dcf880ee52be
Kelly L <lock2846@gmail.com>
2019-05-02 10:20:28 -0600

    Found last letter of lastName

---
+++ 5-Introduction-to-JavaScript/33-Use_Bracket_Notation_to_Find_the_Last_Character_in_a_String.js
@@ -0,0 +1,11 @@
+// To prevent from counting or guessing how many charcters it takes to reach the last character in a string, it can be done by using braket notation and finding the length of the string -1.
+
+// Example
+var firstName = "Ada";
+var lastLetterOfFirstName = firstName[firstName.length - 1];
+
+// Setup
+var lastName = "Lovelace";
+
+// Only change code below this line.
+var lastLetterOfLastName = lastName[lastName.length-1];
