dafa833de1fd8d8686f1acc75a7177aaaa6292da
Kelly L <lock2846@gmail.com>
2019-05-01 20:50:07 -0600

    Used /= operator to find sum

---
+++ 5-Introduction-to-JavaScript/20-Compound-Assignment-With-Augmented-Division.js
@@ -0,0 +1,11 @@
+// The *= operator acts the same as the += operator, only it divides the two numbers.
+
+var a = 48;
+var b = 108;
+var c = 33;
+
+// Only modify code below this line
+
+a /= 12;
+b /= 4;
+c /= 11;
