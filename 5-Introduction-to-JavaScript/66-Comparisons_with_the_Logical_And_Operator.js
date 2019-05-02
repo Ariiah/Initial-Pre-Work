f8000015e1331ba4cbe00bfa6019c9ebf0c8c0b9
Kelly L <lock2846@gmail.com>
2019-05-02 15:35:34 -0600

    Used And operator to check multiple values

---
+++ 5-Introduction-to-JavaScript/66-Comparisons_with_the_Logical_And_Operator.js
@@ -0,0 +1,15 @@
+// The And operator checks for two or more things at once to see if they're true.
+
+function testLogicalAnd(val) {
+  // Only change code below this line
+
+    if (val <= 50 && val >= 25) {
+      return "Yes";
+    }
+
+  // Only change code above this line
+  return "No";
+}
+
+// Change this value to test
+testLogicalAnd(10);
