c64fd7429c15a49de3e254e78052c420b665aa47
Kelly L <lock2846@gmail.com>
2019-05-02 10:14:14 -0600

    Found first letter of lastName

---
+++ 5-Introduction-to-JavaScript/30-Use_Bracket_Notation_to_Find_the_First_Character_in_a_String.js
@@ -0,0 +1,14 @@
+// Using bracket notation, the first letter of a string can be found. It is important to remember that strings start at 0.
+
+// Example
+var firstLetterOfFirstName = "";
+var firstName = "Ada";
+
+firstLetterOfFirstName = firstName[0];
+
+// Setup
+var firstLetterOfLastName = "";
+var lastName = "Lovelace";
+
+// Only change code below this line
+firstLetterOfLastName = lastName[0];
