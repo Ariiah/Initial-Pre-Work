c4544f1a384a9e63f9029e0cfa132ef53319deb4
Kelly L <lock2846@gmail.com>
2019-05-02 10:14:14 -0600

    Inserted variable between two strings

---
+++ 5-Introduction-to-JavaScript/27-Constructing_Strings_with_Variables.js
@@ -0,0 +1,9 @@
+// Sometimes a variable will need to be insterted into the string, and that can also be done through concatenating it into the new string.
+
+// Example
+var ourName = "freeCodeCamp";
+var ourStr = "Hello, our name is " + ourName + ", how are you?";
+
+// Only change code below this line
+var myName = "Kelly";
+var myStr = "My name is" + myName + " and I am well!";
