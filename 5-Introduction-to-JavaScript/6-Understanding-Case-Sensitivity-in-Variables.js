c3d86c1188e4851eba72e7beace992264f003d71
Kelly L <lock2846@gmail.com>
2019-05-01 20:19:24 -0600

    Fixed variables to camelCase

---
+++ 5-Introduction-to-JavaScript/6-Understanding-Case-Sensitivity-in-Variables.js
@@ -0,0 +1,11 @@
+// camelCase is best practice when declaring Javascript variables. It makes for clearer understanding, especially when HTML and CSS are mixed into the same file.
+
+// Declarations
+var studlyCapVar;
+var properCamelCase;
+var titleCaseOver;
+
+// Assignments
+studlyCapVar = 10;
+properCamelCase = "A String";
+titleCaseOver = 9000;
