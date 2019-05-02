dd6f7da736f72114233708ebf6481e86bd74af0f
Kelly L <lock2846@gmail.com>
2019-05-01 20:19:24 -0600

    Reassigned variables a and b

---
+++ 5-Introduction-to-JavaScript/3-Storing-Values-with-the-Assignment-Operator.js
@@ -0,0 +1,9 @@
+// Once a variable is declared, it can be assigned to other points of data. It could one of the six basic types of data, or it could be reassigned to other variables.
+
+// Setup
+var a;
+var b = 2;
+
+// Only change code below this line
+var a = 7;
+var b = a;
