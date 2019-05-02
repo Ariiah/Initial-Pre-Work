4526791038f6c30aa17570020a8dcf7edb40345a
Kelly L <lock2846@gmail.com>
2019-05-02 10:14:14 -0600

    Concatenated variable to another string

---
+++ 5-Introduction-to-JavaScript/26-Concatenating_Strings_with_the_Plus_Equals_Operator.js
@@ -0,0 +1,10 @@
+// String concatenation also works by assigning the previous variable and using the += operator to attach another sentence.
+
+// Example
+var ourStr = "I come first. ";
+ourStr += "I come second.";
+
+// Only change code below this line
+
+var myStr = "This is the first sentence. " ;
+myStr += "This is the second sentence."
