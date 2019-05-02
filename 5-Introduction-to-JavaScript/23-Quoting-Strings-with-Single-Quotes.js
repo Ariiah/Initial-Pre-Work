7d256217a921abab78f1a3dc317cda3ba431f9e3
Kelly L <lock2846@gmail.com>
2019-05-02 10:14:14 -0600

    Link now laid out properly

---
+++ 5-Introduction-to-JavaScript/23-Quoting-Strings-with-Single-Quotes.js
@@ -0,0 +1,3 @@
+// As long as one set of single quotes are used within a string (unless there's an apostrophe), starting the string literal with single quotes will let the code pass safely.
+
+var myStr = '<a href="http://www.example.com" target="_blank">Link</a>';
