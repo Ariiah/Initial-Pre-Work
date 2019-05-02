9eb06c3db0f14fa0a6e9f109b3337c80e8bdc5c3
Kelly L <lock2846@gmail.com>
2019-05-02 10:20:27 -0600

    Fixed typo in string

---
+++ 5-Introduction-to-JavaScript/31-Understand_String_Immutability.js
@@ -0,0 +1,8 @@
+// The only way to change a typo in a string is to redefine it because it is immutable.
+
+// Setup
+var myStr = "Jello World";
+
+// Only change code below this line
+
+myStr = "Hello World"; // Fix Me
