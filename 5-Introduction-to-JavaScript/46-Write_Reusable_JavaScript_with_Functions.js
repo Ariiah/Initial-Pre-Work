0c3aeb593099e8e2631773f39078a5bf6f0ca4ea
Kelly L <lock2846@gmail.com>
2019-05-02 12:11:56 -0600

    Created reuseableFunction

---
+++ 5-Introduction-to-JavaScript/46-Write_Reusable_JavaScript_with_Functions.js
@@ -0,0 +1,14 @@
+// Functions are the most common way data gets passed around in code and can be reused.
+
+// Example
+function ourReusableFunction() {
+  console.log("Heyya, World");
+}
+
+ourReusableFunction();
+
+// Only change code below this line
+function reusableFunction() {
+    console.log("Hi World");
+}
+reusableFunction();
