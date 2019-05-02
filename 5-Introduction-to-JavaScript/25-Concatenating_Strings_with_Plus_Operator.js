8646c7ae4b460c1421f3bfd53498cc698a9622d3
Kelly L <lock2846@gmail.com>
2019-05-02 10:14:14 -0600

    Concatenated two strings

---
+++ 5-Introduction-to-JavaScript/25-Concatenating_Strings_with_Plus_Operator.js
@@ -0,0 +1,8 @@
+// The "+" operator is not only used for addition using numbers, but it is also used for "adding" strings together via concatenation.
+
+// Example
+var ourStr = "I come first. " + "I come second.";
+
+// Only change code below this line
+
+var myStr = "This is the start. " + "This is the end.";
