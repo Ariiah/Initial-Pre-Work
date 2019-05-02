096a106e7104c01fe6e7bc597bf5cba717d21ba9
Kelly L <lock2846@gmail.com>
2019-05-02 12:11:57 -0600

    Checking if numbers are not equal

---
+++ 5-Introduction-to-JavaScript/60-Comparison_with_the_Inequality_Operator.js
@@ -0,0 +1,12 @@
+// The != is the "not the same" operator, so if the two values are not the same, it's true.
+
+// Setup
+function testNotEqual(val) {
+  if (val != 99) { // Change this line
+    return "Not Equal";
+  }
+  return "Equal";
+}
+
+// Change this value to test
+testNotEqual(10);
