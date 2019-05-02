07b406924663724dc4eff542f6088cd808538f11
Kelly L <lock2846@gmail.com>
2019-05-01 20:50:07 -0600

    Used *= operator to find sum

---
+++ 5-Introduction-to-JavaScript/19-Compound-Assignment-With-Augmented-Multiplication.js
@@ -0,0 +1,11 @@
+// The *= operator acts the same as the += operator, only it multiplies the two numbers.
+
+var a = 5;
+var b = 12;
+var c = 4.6;
+
+// Only modify code below this line
+
+a *= 5;
+b *= 3;
+c *= 10;
