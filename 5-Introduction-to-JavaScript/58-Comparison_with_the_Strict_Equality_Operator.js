c6cf5a8eb65c396c4fea028b85f94d510b21c73c
Kelly L <lock2846@gmail.com>
2019-05-02 12:11:57 -0600

    Checked if numbers are strictly equal

---
+++ 5-Introduction-to-JavaScript/58-Comparison_with_the_Strict_Equality_Operator.js
@@ -0,0 +1,12 @@
+// The == operator is a looser comparison tool, but the === operator is very strict and must be exactly the same.
+
+// Setup
+function testStrict(val) {
+  if (val === 7) { // Change this line
+    return "Equal";
+  }
+  return "Not Equal";
+}
+
+// Change this value to test
+testStrict(10);
