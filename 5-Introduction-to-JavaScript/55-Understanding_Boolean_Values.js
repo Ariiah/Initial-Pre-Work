8cb8a9bef143308125bd3c4913c112137b352596
Kelly L <lock2846@gmail.com>
2019-05-02 12:11:57 -0600

    Changed return value to true

---
+++ 5-Introduction-to-JavaScript/55-Understanding_Boolean_Values.js
@@ -0,0 +1,11 @@
+// Booleans are another one of the six data types and state whether something is true or false.
+
+
+function welcomeToBooleans() {
+
+// Only change code below this line.
+
+return true; // Change this line
+
+// Only change code above this line.
+}
