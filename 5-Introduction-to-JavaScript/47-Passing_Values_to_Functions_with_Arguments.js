75cb02ea3ac1cdbe6ec8578e515ef378d01e157c
Kelly L <lock2846@gmail.com>
2019-05-02 12:11:56 -0600

    Made fucntionWithArgs with arguments

---
+++ 5-Introduction-to-JavaScript/47-Passing_Values_to_Functions_with_Arguments.js
@@ -0,0 +1,14 @@
+// When calling parameters, it is important to place them in the exact way it was stated when the fucntion was first written.
+
+// Example
+function ourFunctionWithArgs(a, b) {
+  console.log(a - b);
+}
+ourFunctionWithArgs(10, 5); // Outputs 5
+
+// Only change code below this line.
+
+function functionWithArgs(c, d) {
+  console.log(c + d);
+}
+functionWithArgs(1, 2);
