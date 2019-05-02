e9c5cd9c2354e8abb93d47543a13a95d83b4ac63
Kelly L <lock2846@gmail.com>
2019-05-02 12:11:56 -0600

    Returned value via console log

---
+++ 5-Introduction-to-JavaScript/51-Return_a_Value_from_a_Function_with_Return.js
@@ -0,0 +1,15 @@
+// The return statement acts similar to a console.log(). A return statement actually shows on the client side, while a console.log() shows in dev tools.
+
+// Example
+function minusSeven(num) {
+  return num - 7;
+}
+
+// Only change code below this line
+
+function timesFive (num) {
+  return num * 5
+}
+
+console.log(timesFive(3))
+console.log(minusSeven(10));
