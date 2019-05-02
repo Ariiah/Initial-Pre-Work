a1c52ad688c7ab2e04946af766e266fbc75ec3a2
Kelly L <lock2846@gmail.com>
2019-05-02 15:36:48 -0600

    Created dog object

---
+++ 5-Introduction-to-JavaScript/80-Build_JavaScript_Objects.js
@@ -0,0 +1,18 @@
+// Objects are also one of the biggest ways to hold data. All APIs use objects to store data.
+
+// Example
+var ourDog = {
+  "name": "Camper",
+  "legs": 4,
+  "tails": 1,
+  "friends": ["everything!"]
+};
+
+// Only change code below this line.
+
+var myDog = {
+  "name": "Codey",
+  "legs": 4,
+  "tails": 1,
+  "friends": ["family", "neighbors", "other dogs"]
+};
