1afea3691a417c0dfe3c447ae39fbe895cd5b71a
Kelly L <lock2846@gmail.com>
2019-05-02 15:35:34 -0600

    Checked if the values were greater or equal

---
+++ 5-Introduction-to-JavaScript/63-Comparison_with_the_Greater_Than_Or_Equal_To_Operator.js
@@ -0,0 +1,16 @@
+// Much like math, the greater than or equal to operator checks if the number is greater than or equal to the other one.
+
+function testGreaterOrEqual(val) {
+  if (val >= 20) {  // Change this line
+    return "20 or Over";
+  }
+
+  if (val >= 10) {  // Change this line
+    return "10 or Over";
+  }
+
+  return "Less than 10";
+}
+
+// Change this value to test
+testGreaterOrEqual(10);
