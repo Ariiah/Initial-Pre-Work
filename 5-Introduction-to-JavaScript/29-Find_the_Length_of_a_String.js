d7425bd24a4925414a21c2fb0d67413243c75918
Kelly L <lock2846@gmail.com>
2019-05-02 10:14:14 -0600

    Found length of lastName

---
+++ 5-Introduction-to-JavaScript/29-Find_the_Length_of_a_String.js
@@ -0,0 +1,15 @@
+// To find how many characters are in a string, tagging ".length" to the end of it will give the number of characters.
+
+// Example
+var firstNameLength = 0;
+var firstName = "Ada";
+
+firstNameLength = firstName.length;
+
+// Setup
+var lastNameLength = 0;
+var lastName = "Lovelace";
+
+// Only change code below this line.
+
+lastNameLength = lastName.length;
