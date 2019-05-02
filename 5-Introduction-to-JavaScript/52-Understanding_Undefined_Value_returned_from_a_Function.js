7d2f387e11ff980fd3410b3447d8f2b946abad7f
Kelly L <lock2846@gmail.com>
2019-05-02 12:11:56 -0600

    Defined addFive but no return statement

---
+++ 5-Introduction-to-JavaScript/52-Understanding_Undefined_Value_returned_from_a_Function.js
@@ -0,0 +1,16 @@
+// It is important to remember a return or console.log() statement, otherwise an error will be thrown at you.
+
+// Example
+var sum = 0;
+function addThree() {
+  sum = sum + 3;
+}
+
+// Only change code below this line
+
+function addFive() {
+  sum += 5;
+}
+
+// Only change code above this line
+var returnedValue = addFive();
