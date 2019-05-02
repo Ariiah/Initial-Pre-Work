2b8b9dafb47db872260dbefb0b42608b8d7bec3c
Kelly L <lock2846@gmail.com>
2019-05-02 10:20:28 -0600

    Found the number in the first index of array

---
+++ 5-Introduction-to-JavaScript/38-Access_Array_Data_with_Indexes.js
@@ -0,0 +1,11 @@
+// Finding the indexes of arrays are exactly the same as using bracket notation for strings.
+
+// Example
+var ourArray = [50,60,70];
+var ourData = ourArray[0]; // equals 50
+
+// Setup
+var myArray = [50,60,70];
+
+// Only change code below this line.
+var myData = myArray[0]
