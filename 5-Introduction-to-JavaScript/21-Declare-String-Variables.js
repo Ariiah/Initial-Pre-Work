493a997b78548c58d9fdfb9bc98dd4e5645a5f8f
Kelly L <lock2846@gmail.com>
2019-05-02 10:14:14 -0600

    Assigned strings to variables

---
+++ 5-Introduction-to-JavaScript/21-Declare-String-Variables.js
@@ -0,0 +1,9 @@
+// A string literal is defined my quotation marks.
+
+// Example
+var firstName = "Alan";
+var lastName = "Turing";
+
+// Only change code below this line
+var myFirstName = "Kelly"
+var myLastName = "Lockwood"
