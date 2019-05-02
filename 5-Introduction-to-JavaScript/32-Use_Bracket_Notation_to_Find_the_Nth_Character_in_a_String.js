996264b68420a48734fbd24484a6074ee7fe1278
Kelly L <lock2846@gmail.com>
2019-05-02 10:20:28 -0600

    Found third letter of lastName

---
+++ 5-Introduction-to-JavaScript/32-Use_Bracket_Notation_to_Find_the_Nth_Character_in_a_String.js
@@ -0,0 +1,11 @@
+// Starting from 0, you can find which characters are at which indexes. Although, if the index is larger than the amount of characters, it will throw an undefined.
+
+// Example
+var firstName = "Ada";
+var secondLetterOfFirstName = firstName[1];
+
+// Setup
+var lastName = "Lovelace";
+
+// Only change code below this line.
+var thirdLetterOfLastName = lastName[2];
