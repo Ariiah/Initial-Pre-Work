5a440de54b61b2f9230f66e62eb9e889d1ad77e0
Kelly L <lock2846@gmail.com>
2019-05-01 20:50:07 -0600

    Used -= operator to find sum

---
+++ 5-Introduction-to-JavaScript/18-Compound-Assignment-With-Augmented-Subtraction.js
@@ -0,0 +1,11 @@
+// Similar to the += operator, the -= operator subtracts the number from the assigned variable to the number given.
+
+var a = 11;
+var b = 9;
+var c = 3;
+
+// Only modify code below this line
+
+a -= 6;
+b -= 15;
+c -= 1;
