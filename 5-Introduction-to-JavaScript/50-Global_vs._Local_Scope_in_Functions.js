210118d8c6b7e47931da76d2f14f2e2e1bd3e142
Kelly L <lock2846@gmail.com>
2019-05-02 12:11:56 -0600

    Redefined outerWear variable

---
+++ 5-Introduction-to-JavaScript/50-Global_vs._Local_Scope_in_Functions.js
@@ -0,0 +1,15 @@
+// Local variables override global variables when they use the same name.
+
+// Setup
+var outerWear = "T-Shirt";
+
+function myOutfit() {
+  // Only change code below this line
+
+  var outerWear = "sweater"
+
+  // Only change code above this line
+  return outerWear;
+}
+
+myOutfit();
