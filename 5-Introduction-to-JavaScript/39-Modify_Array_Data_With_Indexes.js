b2da5f7ead3332fda669fb85eaaea599e2527978
Kelly L <lock2846@gmail.com>
2019-05-02 10:20:28 -0600

    Changed number in first index

---
+++ 5-Introduction-to-JavaScript/39-Modify_Array_Data_With_Indexes.js
@@ -0,0 +1,12 @@
+// The data in arrays in mutable, meaning they are able to be changed, unlike strings. But if a broken up sentence were put inside an array, then it could be changed.
+
+// Example
+var ourArray = [18,64,99];
+ourArray[1] = 45; // ourArray now equals [18,45,99].
+
+// Setup
+var myArray = [18,64,99];
+
+// Only change code below this line.
+
+myArray[0] = 45
