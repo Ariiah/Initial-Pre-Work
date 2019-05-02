4b55a1a55af3437b25236758cd8013b95978a587
Kelly L <lock2846@gmail.com>
2019-05-02 12:11:57 -0600

    Checked if numbers are equal to each other

---
+++ 5-Introduction-to-JavaScript/57-Comparison_with_the_Equality_Operator.js
@@ -0,0 +1,12 @@
+// While the = operator is used to assign something, the == is used to check is something is the same.
+
+// Setup
+function testEqual(val) {
+  if (val == 12) { // Change this line
+    return "Equal";
+  }
+  return "Not Equal";
+}
+
+// Change this value to test
+testEqual(10);
