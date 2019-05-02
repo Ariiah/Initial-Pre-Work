728e4f6cdd9c50487931d5a890dfc9f30555b99d
Kelly L <lock2846@gmail.com>
2019-05-02 10:20:28 -0600

    Created array

---
+++ 5-Introduction-to-JavaScript/36-Store_Multiple_Values_in_one_Variable_using_JavaScript_Arrays.js
@@ -0,0 +1,7 @@
+// Arrays are one of the most common objects that are used in Javascript to hold data.
+
+// Example
+var ourArray = ["John", 23];
+
+// Only change code below this line.
+var myArray = ["cookies", 13];
