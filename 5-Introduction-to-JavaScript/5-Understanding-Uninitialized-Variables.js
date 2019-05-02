f8b85f60068192bd90026ebf519b4ace6d9f539b
Kelly L <lock2846@gmail.com>
2019-05-01 20:19:24 -0600

    Variables a b and c have values

---
+++ 5-Introduction-to-JavaScript/5-Understanding-Uninitialized-Variables.js
@@ -0,0 +1,12 @@
+// If there is nothing assigned to a variable, the code will throw an undefined error. Even when the variable is reassigned to add more data, it takes the previous declaration, and if there is no data to pull from, the undefined error will still be thrown.
+
+// Initialize these three variables
+var a = 5;
+var b = 10;
+var c = "I am a";
+
+// Do not change code below this line
+
+a = a + 1;
+b = b + 5;
+c = c + " String!";
