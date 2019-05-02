b041b540caedf9643d125273ea6d919de40ae3f0
Kelly L <lock2846@gmail.com>
2019-05-01 20:19:24 -0600

    Added one lined and multi lined comments

--- 5-Introduction-to-JavaScript/1-Comment-Your-JavaScript-Code.js
+++ 5-Introduction-to-JavaScript/1-Comment-Your-JavaScript-Code.js
@@ -1,3 +1,7 @@
-// Please remember to include a summary statement with EACH FILE regarding what you learned from the lesson in this format!  I recommend keeping it at the top of each file for faster processing of your application.
+// Javascript has two ways in which to leave a comment. One line comments are two forward slashes, while multi-line comments are the same as CSS comments.
 
-// Copy your code from the central panel of the freeCodeCamp page and paste it below after successfully passing the test.  You will have to click on the X at the top right of the modal that pops up to see the code. If you forget, just rerun the test.  You are also welcome to download your solution.
+// I take up one line!
+
+/* Javascript is a
+source of creation which gives
+function to a site */
