95d4a977ed017a1b924244489e3c48cd0bd65333
Kelly L <lock2846@gmail.com>
2019-05-02 10:20:28 -0600

    Found second to last letter of lastName

---
+++ 5-Introduction-to-JavaScript/34-Use_Bracket_Notation_to_Find_the_Nth-to-Last_Character_in_a_String.js
@@ -0,0 +1,11 @@
+// Starting from the end of a string can also work backwards when trying to find the indexes of the last characters.
+
+// Example
+var firstName = "Ada";
+var thirdToLastLetterOfFirstName = firstName[firstName.length - 3];
+
+// Setup
+var lastName = "Lovelace";
+
+// Only change code below this line
+var secondToLastLetterOfLastName = lastName[lastName.length-2];
