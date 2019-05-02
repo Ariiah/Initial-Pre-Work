45113fbc1116f126834301da3cb2dec00eef2bd2
Kelly L <lock2846@gmail.com>
2019-05-01 20:50:06 -0600

    Found the remainder of 11 to 3

---
+++ 5-Introduction-to-JavaScript/16-Finding-a-Remainder-in-JavaScript.js
@@ -0,0 +1,5 @@
+// The modulo operator is used to find the remainder of a number, not divide. It is also not to be confused with a percentage sign.
+
+// Only change code below this line
+
+var remainder = 11 % 3;
