705b805403abee749c9fc1aed32566cd25e008d3
Kelly L <lock2846@gmail.com>
2019-05-02 10:14:14 -0600

    Concatenated both string via variables

---
+++ 5-Introduction-to-JavaScript/28-Appending_Variables_to_Strings.js
@@ -0,0 +1,12 @@
+// Concatenating strings via variables can also be added, or appended, together as well.
+
+// Example
+var anAdjective = "awesome!";
+var ourStr = "freeCodeCamp is ";
+ourStr += anAdjective;
+
+// Only change code below this line
+
+var someAdjective = "a journey." ;
+var myStr = "Learning to code is ";
+myStr += someAdjective;
