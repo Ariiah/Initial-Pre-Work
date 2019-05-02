237d7088f21f044f864d3d0d722cd726ccc8bb40
Kelly L <lock2846@gmail.com>
2019-05-02 10:14:14 -0600

    String now shows quotes properly

---
+++ 5-Introduction-to-JavaScript/22-Escaping-Literal-Quotes-in-Strings.js
@@ -0,0 +1,3 @@
+// To prevent from the code not processing quotes within a string literal, a backslash is used before the quote at the beginning and before the last quotation mark.
+
+var myStr = "I am a \"double quoted\" string inside \"double quotes\".";
