6e393617bf5fb9cf40bb809d63faabd8280a2d49
Kelly L <lock2846@gmail.com>
2019-05-02 15:35:34 -0600

    Added and else statement

---
+++ 5-Introduction-to-JavaScript/68-Introducing_Else_Statements.js
@@ -0,0 +1,20 @@
+// The Else statement is a way to catch the code if the If statement doen't go through.
+
+function testElse(val) {
+  var result = "";
+  // Only change code below this line
+
+  if (val > 5) {
+    result = "Bigger than 5";
+  }
+
+  else {
+    result = "5 or Smaller";
+  }
+
+  // Only change code above this line
+  return result;
+}
+
+// Change this value to test
+testElse(4);
