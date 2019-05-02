b254db7ae0cb9e52acba6ab705dcc04efab5615f
Kelly L <lock2846@gmail.com>
2019-05-02 15:36:48 -0600

    Used operator for boolean output

---
+++ 5-Introduction-to-JavaScript/77-Returning_Boolean_Values_from_Functions.js
@@ -0,0 +1,9 @@
+// The shorthand way to use a boolean value is to strictly compare or use greater than or equal to the arguments.
+
+function isLess(a, b) {
+  // Fix this code
+  return a <= b
+}
+
+// Change these values to test
+isLess(10, 15);
