3f946b3cb5bf16d18b0a4e5aa79a2ba97fe72a8a
Kelly L <lock2846@gmail.com>
2019-05-02 15:35:34 -0600

    Reordered conditionals

---
+++ 5-Introduction-to-JavaScript/70-Logical_Order_in_If_Else_Statements.js
@@ -0,0 +1,14 @@
+// To make the If Else statement work, the most complicated step needs to go first, otherwise, the first step may pass through without checking the other conditions.
+
+function orderMyLogic(val) {
+  if (val < 5) {
+    return "Less than 5";
+  } else if (val < 10) {
+    return "Less than 10";
+  } else {
+    return "Greater than or equal to 10";
+  }
+}
+
+// Change this value to test
+orderMyLogic(7);
