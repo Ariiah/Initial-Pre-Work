1d9d0594e76e505e1d25115863bfd5fe7e23b93e
Kelly L <lock2846@gmail.com>
2019-05-01 20:19:24 -0600

    Declared myName variable

---
+++ 5-Introduction-to-JavaScript/2-Declare-JavaScript-Variables.js
@@ -0,0 +1,7 @@
+// Declaring variables gives the ability to store and manipulate data. These variables make it easier to follow the flow of the code and what each piece of information means.
+
+// Example
+var ourName;
+
+// Declare myName below this line
+var myName;
