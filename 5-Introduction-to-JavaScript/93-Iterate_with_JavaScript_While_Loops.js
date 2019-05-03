a65fd0c602e2ea77ca96c47621a4eac4206fcf68
Kelly L <lock2846@gmail.com>
2019-05-02 18:02:20 -0600

    Do while loop test

---
+++ 5-Introduction-to-JavaScript/93-Iterate_with_JavaScript_While_Loops.js
@@ -0,0 +1,11 @@
+// While loops are a good way to prevent from copying more code than nessessary. Although, if not stated correctly, it will get stick in an infinate loop.
+
+// Setup
+var myArray = [];
+
+// Only change code below this line.
+var i = 0
+while (i < 5) {
+  myArray.push(i);
+  i++;
+}
