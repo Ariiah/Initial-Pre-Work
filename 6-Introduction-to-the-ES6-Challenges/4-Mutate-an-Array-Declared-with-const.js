333dc00213dbecc44bbabed6a9a6e33c1fd613d2
Kelly L <lock2846@gmail.com>
2019-05-02 18:11:23 -0600

    Changed array values

---
+++ 6-Introduction-to-the-ES6-Challenges/4-Mutate-an-Array-Declared-with-const.js
@@ -0,0 +1,16 @@
+// Since the const variable cannot be directly redefined, redefining parts of the array are.
+
+const s = [5, 7, 2];
+function editInPlace() {
+  "use strict";
+  // change code below this line
+
+  // s = [2, 5, 7]; <- this is invalid
+
+  s[0] = 2
+  s[1] = 5
+  s[2] = 7
+
+  // change code above this line
+}
+editInPlace();
