87b8cf827e1cdbdde7651cbf7d71c0635e94c1df
Kelly L <lock2846@gmail.com>
2019-05-03 10:01:16 -0600

    Removed repeating variables

---
+++ 6-Introduction-to-the-ES6-Challenges/18-Write-Concise-Object-Literal-Declarations-Using-Simple-Fields.js
@@ -0,0 +1,13 @@
+// Instead of repeating the same variable twice, the code know how to read through it once and fill it in the from there.
+
+const createPerson = (name, age, gender) => {
+  "use strict";
+  // change code below this line
+  return {
+    name,
+    age,
+    gender
+  };
+  // change code above this line
+};
+console.log(createPerson("Zodiac Hasbro", 56, "male")); // returns a proper object
