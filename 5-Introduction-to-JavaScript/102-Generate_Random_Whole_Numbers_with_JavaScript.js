91d172c31df33d49d5c192ac7ca402efb8dd7bec
Kelly L <lock2846@gmail.com>
2019-05-02 19:10:04 -0600

    Function returns random whole number

---
+++ 5-Introduction-to-JavaScript/102-Generate_Random_Whole_Numbers_with_JavaScript.js
@@ -0,0 +1,10 @@
+// The Math.floor() only generates numbers between 0 and 1 by default. In order use higher numbers, multiplying it by the number you want will accomplish this.
+
+var randomNumberBetween0and19 = Math.floor(Math.random() * 20);
+
+function randomWholeNum() {
+
+  // Only change code below this line.
+
+  return Math.floor(Math.random() * 10);
+}
