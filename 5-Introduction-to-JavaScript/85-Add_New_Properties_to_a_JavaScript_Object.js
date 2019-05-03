dd1f16394bf3f465d813b3a716ada4e81f262e18
Kelly L <lock2846@gmail.com>
2019-05-02 18:02:20 -0600

    Added new properties in object

---
+++ 5-Introduction-to-JavaScript/85-Add_New_Properties_to_a_JavaScript_Object.js
@@ -0,0 +1,22 @@
+// A new key value can also be added to the object, and it attaches at the end of it.
+
+// Example
+var ourDog = {
+  "name": "Camper",
+  "legs": 4,
+  "tails": 1,
+  "friends": ["everything!"]
+};
+
+ourDog.bark = "bow-wow";
+
+// Setup
+var myDog = {
+  "name": "Happy Coder",
+  "legs": 4,
+  "tails": 1,
+  "friends": ["freeCodeCamp Campers"]
+};
+
+// Only change code below this line.
+myDog["bark"] = "bork"
