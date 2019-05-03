82516a3aee36a40b0020ba4e6844012039cf558c
Kelly L <lock2846@gmail.com>
2019-05-02 19:10:04 -0600

    Converts number to binary

---
+++ 5-Introduction-to-JavaScript/105-Use_the_parseInt_Function_with_a_Radix.js
@@ -0,0 +1,7 @@
+// Adding the radix parameter gives the option to show if the number exsists in the particular numeric system stated.
+
+function convertToInteger(str) {
+  return parseInt(str, 2)
+}
+
+convertToInteger("10011");
