873180e3b2d7f43e8502d4e8572ae022ccae58d9
Kelly L <lock2846@gmail.com>
2019-05-02 18:11:23 -0600

    Chained map and filter to find result

---
+++ 6-Introduction-to-the-ES6-Challenges/8-Write-Higher-Order-Arrow-Functions.js
@@ -0,0 +1,14 @@
+// You can chain higher order functions to do multiple steps at once.
+
+const realNumberArray = [4, 5.6, -9.8, 3.14, 42, 6, 8.34, -2];
+const squareList = (arr) => {
+  "use strict";
+  // change code below this line
+  const squaredIntegers = arr.filter(positiveNum => positiveNum > 0 && Number.isInteger(positiveNum)).map(num => Math.pow(num, 2));
+
+  // change code above this line
+  return squaredIntegers;
+};
+// test your code
+const squaredIntegers = squareList(realNumberArray);
+console.log(squaredIntegers);
