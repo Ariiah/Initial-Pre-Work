4075fc45c571a814f95d335666f5f8f965483f27
Kelly L <lock2846@gmail.com>
2019-05-02 18:02:21 -0600

    Backward array made via for loop

--- 5-Introduction-to-JavaScript/96-Count_Backwards_With_a_For_Loop.js
+++ 5-Introduction-to-JavaScript/96-Count_Backwards_With_a_For_Loop.js
@@ -1,1 +1,16 @@
+// For loops can also count backwards as well as skip numbers backwards.
 
+// Example
+var ourArray = [];
+
+for (var i = 10; i > 0; i -= 2) {
+  ourArray.push(i);
+}
+
+// Setup
+var myArray = [];
+
+// Only change code below this line.
+for(var i = 9; i > 0; i -= 2){
+  myArray.push(i)
+}
