d52645095ff671ba50e2871aff3d68c5e0622213
Kelly L <lock2846@gmail.com>
2019-05-03 10:01:16 -0600

    Exported variables separately

---
+++ 6-Introduction-to-the-ES6-Challenges/23-Use-export-to-Reuse-a-Code-Block.js
@@ -0,0 +1,7 @@
+// Export allows the functions or declarations to be used outside of the file it's in.
+
+"use strict";
+export const foo = "bar";
+export const bar = "foo";
+
+// export { foo, bar };
