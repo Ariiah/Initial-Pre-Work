dbb9b0749d25d3ba127633b02783f5b8c4332242
Kelly L <lock2846@gmail.com>
2019-05-02 18:02:20 -0600

    Accessed object via bracket notation

---
+++ 5-Introduction-to-JavaScript/82-Accessing_Object_Properties_with_Bracket_Notation.js
@@ -0,0 +1,13 @@
+// Objects can be navigated using bracket notation as well, although dot notation is cleaner when bigger object are involved.
+
+// Setup
+var testObj = {
+  "an entree": "hamburger",
+  "my side": "veggies",
+  "the drink": "water"
+};
+
+// Only change code below this line
+
+var entreeValue = testObj["an entree"];   // Change this line
+var drinkValue = testObj["the drink"];    // Change this line
