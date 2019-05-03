3cae008609a533d1804717f16da97687d1275b6f
Kelly L <lock2846@gmail.com>
2019-05-02 18:02:21 -0600

    For loop test

---
+++ 5-Introduction-to-JavaScript/94-Iterate_with_JavaScript_For_Loops.js
@@ -0,0 +1,17 @@
+// The for loop is the most common way to loop through a piece of code. It takes a string, array, or object to go through a specific number of times.
+
+// Example
+var ourArray = [];
+
+for (var i = 0; i < 5; i++) {
+  ourArray.push(i);
+}
+
+// Setup
+var myArray = [];
+
+// Only change code below this line.
+
+for(var i = 1; i < 6; i++) {
+  myArray.push(i)
+}
