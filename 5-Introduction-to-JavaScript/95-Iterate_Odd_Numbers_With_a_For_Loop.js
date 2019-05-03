2f76e984fa8413246e076dddc4b9fb42b37cbb39
Kelly L <lock2846@gmail.com>
2019-05-02 18:02:21 -0600

    Pushed new data to array via for loop

---
+++ 5-Introduction-to-JavaScript/95-Iterate_Odd_Numbers_With_a_For_Loop.js
@@ -0,0 +1,17 @@
+// By specifying what the final expression is, it will start at 1, and then return every other character, in this case.
+
+// Example
+var ourArray = [];
+
+for (var i = 0; i < 10; i += 2) {
+  ourArray.push(i);
+}
+
+// Setup
+var myArray = [];
+
+// Only change code below this line.
+
+for(var i = 1; i < 10; i += 2) {
+  myArray.push(i)
+}
