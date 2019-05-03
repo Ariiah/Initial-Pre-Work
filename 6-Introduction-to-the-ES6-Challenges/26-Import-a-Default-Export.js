25be9369f4f9111097d0956a79a5b3295b41d4cd
Kelly L <lock2846@gmail.com>
2019-05-03 10:01:16 -0600

    Imported subtract function

---
+++ 6-Introduction-to-the-ES6-Challenges/26-Import-a-Default-Export.js
@@ -0,0 +1,5 @@
+// The import syntax, in this case, is pulling from the previous file (exercise) and using its function to solve the subtraction problem.
+
+"use strict";
+import subtract from "math_functions";
+subtract(7,4);
