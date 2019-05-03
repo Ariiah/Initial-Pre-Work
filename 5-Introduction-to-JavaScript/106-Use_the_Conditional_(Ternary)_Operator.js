f52ab1126c56224ebf69c1a4955620323a0c01fc
Kelly L <lock2846@gmail.com>
2019-05-02 19:10:05 -0600

    Used ternary operator to find if numbers are equal

---
+++ 5-Introduction-to-JavaScript/106-Use_the_Conditional_(Ternary)_Operator.js
@@ -0,0 +1,7 @@
+// Ternary operators are useful when saving space in the code. It is a shorthand way of doing if/else statement.
+
+function checkEqual(a, b) {
+  return a == b ? true : false
+}
+
+checkEqual(1, 2);
