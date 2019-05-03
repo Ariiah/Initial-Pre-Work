8420a900b529fc9358b2d0a1b705ef4339490f7b
Kelly L <lock2846@gmail.com>
2019-05-03 10:01:16 -0600

    Imported needed module

---
+++ 6-Introduction-to-the-ES6-Challenges/22-Understand-the-Differences-Between-import-and-require.js
@@ -0,0 +1,5 @@
+// The import statement allows to pull the modules needed from the module file as opposed to the whole file.
+
+"use strict";
+import { capitalizeString } from "string-functions";
+capitalizeString("hello!");
