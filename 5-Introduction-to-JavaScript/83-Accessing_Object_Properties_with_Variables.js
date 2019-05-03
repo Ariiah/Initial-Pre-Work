7f40d1e496b8492c154b154fc14c3a945efb999e
Kelly L <lock2846@gmail.com>
2019-05-02 18:02:20 -0600

    Used variables to find data in object

---
+++ 5-Introduction-to-JavaScript/83-Accessing_Object_Properties_with_Variables.js
@@ -0,0 +1,13 @@
+// By defining a variable to an object's key and then using it via bracket notation, it can making finding information easier.
+
+// Setup
+var testObj = {
+  12: "Namath",
+  16: "Montana",
+  19: "Unitas"
+};
+
+// Only change code below this line;
+
+var playerNumber = 16;       // Change this Line
+var player = testObj[playerNumber];   // Change this Line
