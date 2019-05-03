9c240606320cc33335994af65c5bfc3d3fc7b0b3
Kelly L <lock2846@gmail.com>
2019-05-02 18:02:20 -0600

    Added new artist and info into object

---
+++ 5-Introduction-to-JavaScript/89-Manipulating_Complex_Objects.js
@@ -0,0 +1,25 @@
+// Complex objects are a good way to hold different types of data in one place. 
+
+var myMusic = [
+  {
+    "artist": "Billy Joel",
+    "title": "Piano Man",
+    "release_year": 1973,
+    "formats": [
+      "CD",
+      "8T",
+      "LP"
+    ],
+    "gold": true
+  },
+  // Add record here
+  {
+  "artist": "Owl City",
+  "title": "Hot Air Balloon",
+  "release_year": 2009,
+  "formats": [
+    "CD",
+    "Single"
+  ]
+  }
+];
