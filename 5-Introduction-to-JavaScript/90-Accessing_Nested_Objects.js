02a6d33fc7078beb246a133eacfbb040b4c82cdc
Kelly L <lock2846@gmail.com>
2019-05-02 18:02:20 -0600

    Found glove box data via dot and bracket notation

---
+++ 5-Introduction-to-JavaScript/90-Accessing_Nested_Objects.js
@@ -0,0 +1,16 @@
+// By using a combination of dot and bracket notation, naviagation through nested objects is easy!
+
+// Setup
+var myStorage = {
+  "car": {
+    "inside": {
+      "glove box": "maps",
+      "passenger seat": "crumbs"
+     },
+    "outside": {
+      "trunk": "jack"
+    }
+  }
+};
+
+var gloveBoxContents = myStorage.car.inside["glove box"]; // Change this line
