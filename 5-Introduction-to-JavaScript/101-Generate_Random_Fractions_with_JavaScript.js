b818afef19d32ac453a64d389b2b61e956a7be1c
Kelly L <lock2846@gmail.com>
2019-05-02 19:10:04 -0600

    Function returns random fraction

---
+++ 5-Introduction-to-JavaScript/101-Generate_Random_Fractions_with_JavaScript.js
@@ -0,0 +1,10 @@
+// The Math.random() function is a more feasible way to randomize numbers. It's better than assigning an array with random numbers where there's only a set amount.
+
+function randomFraction() {
+
+  // Only change code below this line.
+
+  return Math.random();
+
+  // Only change code above this line.
+}
