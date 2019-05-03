efec4cde74e09d290c175155c3a0dedd0cff79b7
Kelly L <lock2846@gmail.com>
2019-05-03 10:01:16 -0600

    Imported all content from previous file

---
+++ 6-Introduction-to-the-ES6-Challenges/24-Use-Asterisk-to-Import-Everything-from-a-File.js
@@ -0,0 +1,4 @@
+// The import * as syntax allows to bring all of an outside file (or an object, in this case), name it, and the path to where the file is.
+
+"use strict";
+import * as str from "capitalize_strings";
